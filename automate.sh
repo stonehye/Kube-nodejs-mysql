822bc6d4ce650a328ab082f61668f6cedbb147fc
stonehye <stonehye@gmail.com>
2021-07-26 16:46:04 +0900

    Add configmap and modify values

--- automate.sh
+++ automate.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 kubectl create -f ./secrets.yml
+kubectl create -f ./configmap.yml
 kubectl create -f ./mysql-compose.yml
 kubectl create -f ./mysql-service.yml
 kubectl create -f ./nodejs-mysql-deployment.yml
 kubectl create -f ./nodejs-service.yml
-kubectl get services
-minikube service spring-service --url
