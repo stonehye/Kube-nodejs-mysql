18fac8c758b86f33db520e9890537006ba4f92ec
stonehye <stonehye@gmail.com>
2021-08-02 11:25:10 +0900

    update PV

--- automate.sh
+++ automate.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 kubectl create -f ./secrets.yml
 kubectl create -f ./configmap.yml
-kubectl create -f ./mysql-compose.yml
+kubectl create -f ./mysql-pv.yaml
 kubectl create -f ./mysql-service.yml
+kubectl create -f ./mysql-compose.yml
+sleep 10s
 kubectl create -f ./nodejs-mysql-deployment.yml
 kubectl create -f ./nodejs-service.yml
