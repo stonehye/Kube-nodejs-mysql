9d30f99949f8ea7c1a69855a52d15729ce2a9164
Hyekyoung Seok <hyekyoung_seok@tmax.co.kr>
2021-08-11 11:29:12 +0900

    modified

--- automate.sh
+++ automate.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-kubectl create -f ./secrets.yml
-kubectl create -f ./configmap.yml
-kubectl create -f ./mysql-pv.yaml
-kubectl create -f ./mysql-service.yml
-kubectl create -f ./mysql-compose.yml
-sleep 10s
-kubectl create -f ./nodejs-mysql-deployment.yml
-kubectl create -f ./nodejs-service.yml
+kubectl apply -f ./namespace.yaml
+kubectl apply -f ./secrets.yaml
+kubectl apply -f ./configmap.yaml
+kubectl apply -f ./mysql-pv.yaml
+kubectl apply -f ./mysql.yaml
+# sleep 240s
+kubectl apply -f ./waplshop-app.yaml
